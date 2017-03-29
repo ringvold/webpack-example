ae2052277a8a274074cbc513c94c41896ca7e482
Harald Ringvold <harald.ringvold@vegvesen.no>
2017-03-29 10:24:48 +0200

    Testing list problem

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ import Snow from 'quill/themes/snow';
 import Bold from 'quill/formats/bold';
 import Italic from 'quill/formats/italic';
 import Header from 'quill/formats/header';
-
+import List from 'quill/formats/list';
+import ListItem from 'quill/formats/list';
 
 Quill.register({
-  'modules/toolbar': Toolbar,
-  'themes/snow': Snow,
-  'formats/bold': Bold,
-  'formats/italic': Italic,
-  'formats/header': Header
+    'modules/toolbar': Toolbar,
+    'themes/snow': Snow,
+    'formats/bold': Bold,
+    'formats/italic': Italic,
+    'formats/header': Header,
+    'formats/list': List,
+    'formats/list-item': ListItem,
 });
 
+console.log('Starter Quill');
+var quill = new Quill('#quill', { theme: 'snow' });
 
 export default Quill;
